894a0b2af739f8a1e826649fbf7cfc194f96bb19
Mechibot7 <84905641+Mechibot7@users.noreply.github.com>
2022-07-19 21:05:03 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['601163276045',''] //owner number +27634090203
+global.wame = ['601163276045'] 
+global.pemilik = ['601163276045'] //owner number +27634090203
+global.premium = ['601163276045'] //premium number +27634090203
+global.pengguna = 'Matyu' //username
+global.footer = 'JANDA BOT INC'
+global.botnma = 'JANDA BOT INC' //bot name
+global.botname = 'JANDA BOT V4'
+global.ownernma = 'Matyu' //owner name
+global.packname = 'Matyu' //sticker package name
+global.author = 'By Matyu Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
